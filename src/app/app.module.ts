eeb1b6c48bcce5532d305215dd03456b751d7910
gRoussac <gRoussac@users.noreply.github.com>
2022-09-23 13:19:16 +0200

    add some magic import.meta.url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { AppComponent } from './app.component';
     {
       provide: FIBONACCI_WEBWORKER_FACTORY,
       useValue: function (): Worker {
-        return new Worker('projects/fibonacci-webworker/src/lib/fibonacci', {
+        return new Worker(new URL('projects/fibonacci-webworker/src/lib/fibonacci', import.meta.url), {
           name: 'fibonacci.worker',
           type: 'module',
         });
